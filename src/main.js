50261309b9df9041c7e1617499f793ac346ba958
Julia <zakr.jv@gmail.com>
2021-04-07 18:44:55 +0800

    Рендерит фильмы при нажатии show more

--- src/main.js
+++ src/main.js
@@ -1,22 +1,23 @@
 import {createFilterMenu} from './view/filters.js';
 import {createSortMenu} from './view/sort.js';
 import {createUserRank} from './view/user-rank.js';
+import {createFooterStatistics} from './view/footer.js';
 import {createFilmsTemplate} from './view/films-template.js';
 import {createFilmCard} from './view/film-card.js';
 import {createButtonShowMore} from './view/button-show-more';
-import {createPopupFilmDetails} from './view/film-details-popup.js';
+// import {createPopupFilmDetails} from './view/film-details-popup.js';
 import {generateFilmCard} from './mock/film-card.js';
-import {createFilmComment} from './view/film-comments.js';
-import {generateFilmComments} from './mock/film-comments.js';
+// import {createFilmComment} from './view/film-comments.js';
+// import {generateFilmComments} from './mock/film-comments.js';
 import {generateFilters} from './mock/filter.js';
 
-const FILMS_COUNT = 5;
+const FILMS_COUNT = 30;
+const FILMS_COUNT_PER_STEP = 5;
 const EXTRA_COUNT = 2;
 
 const films = new Array(FILMS_COUNT).fill(null).map(generateFilmCard);
-const comments = new Array(films[0].commentsCount).fill(null).map(generateFilmComments);
+// const comments = new Array(films[0].commentsCount).fill(null).map(generateFilmComments);
 const filters = generateFilters(films);
-console.log(generateFilters(films));
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -42,12 +43,31 @@ const filmListMostCommented = filmsElement.querySelector('.films-list--most-comm
 
 // Films list
 const filmListContainerFilms = filmListAllFilms.querySelector('.films-list__container');
-for (let i = 0; i < FILMS_COUNT; i++) {
+for (let i = 0; i < Math.min(films.length, FILMS_COUNT_PER_STEP); i++) {
   render(filmListContainerFilms, createFilmCard(films[i]), 'beforeend');
 }
 
 // Show more button
-render(filmListAllFilms, createButtonShowMore(), 'beforeend');
+if (films.length > FILMS_COUNT_PER_STEP) {
+  let renderedTaskCount = FILMS_COUNT_PER_STEP;
+
+  render(filmListAllFilms, createButtonShowMore(), 'beforeend');
+
+  const showMoreButton = document.querySelector('.films-list__show-more');
+
+  showMoreButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    films
+      .slice(renderedTaskCount, renderedTaskCount + FILMS_COUNT_PER_STEP)
+      .forEach((film) => render(filmListContainerFilms, createFilmCard(film), 'beforeend'));
+
+    renderedTaskCount += FILMS_COUNT_PER_STEP;
+
+    if (renderedTaskCount >= films.length) {
+      showMoreButton.remove();
+    }
+  });
+}
 
 // Top rated
 const filmListContainerTopRated = filmListTopRated.querySelector('.films-list__container');
@@ -60,6 +80,10 @@ const filmListContainerMostCommented = filmListMostCommented.querySelector('.fil
 for (let i = 0; i < EXTRA_COUNT; i++) {
   render(filmListContainerMostCommented, createFilmCard(films[i]), 'beforeend');
 }
+
+// Footer
+const footerElement = document.querySelector('.footer');
+render(footerElement, createFooterStatistics(films.length), 'beforeend');
 
 // // Popup
 // const bodyElement = document.body;

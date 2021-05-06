517a3be57ede72976a093330db3f9de8938e837d
Julia <zakr.jv@gmail.com>
2021-05-06 14:33:02 +0800

    Поправляет шаблон сортировки

--- src/view/site-menu-sort.js
+++ src/view/site-menu-sort.js
@@ -1,10 +1,11 @@
 import AbstractView from '../view/abstract.js';
+import {SortType} from '../const.js';
 
 const createSortMenu = () => {
   return `<ul class="sort">
-  <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-  <li><a href="#" class="sort__button">Sort by date</a></li>
-  <li><a href="#" class="sort__button">Sort by rating</a></li>
+  <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+  <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE}">Sort by date</a></li>
+  <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
 </ul>`;
 };
 
